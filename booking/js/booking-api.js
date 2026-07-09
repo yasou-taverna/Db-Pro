32f2885796e25bce54032ea638baee4b97a85c8f
Yasou Taverna <yasoutavern@gmail.com>
2026-07-09 08:35:40 -0400

    Fix formatting in booking-api.js

--- booking/js/booking-api.js
+++ booking/js/booking-api.js
@@ -1,7 +1,7 @@
 export const API_URL = 'https://script.google.com/macros/s/AKfycbynI5CHvZhKXx2f-RgVAXkAa0c4IZce6Wz-FjLcqrsP05i-vWskwvX6Vdq-pY546vp3/exec';
 
 export async function postReservation(reservation) {
-  const response = await fetch(API_URL, {
+  const response = await fetch(API_URL, { 
     method: 'POST',
     body: JSON.stringify({
       action: 'addReservation',
